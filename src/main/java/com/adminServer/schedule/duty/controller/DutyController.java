a210a4cbd758aaa4886497911882a54c791dbbb7
inyoung <charmingyear@gmail.com>
2023-07-28 15:43:16 +0900

    fix: 당직 신청 리스트 조회 반환값 수정 (#5)

--- src/main/java/com/adminServer/schedule/duty/controller/DutyController.java
+++ src/main/java/com/adminServer/schedule/duty/controller/DutyController.java
@@ -1,10 +1,12 @@
 package com.adminServer.schedule.duty.controller;
 
+import com.adminServer._core.util.ApiResponse;
 import com.adminServer.schedule.duty.dto.DutyResponse;
 import com.adminServer.schedule.duty.service.DutyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -16,8 +18,9 @@ public class DutyController {
     private final DutyService dutyService;
 
     @GetMapping("/{status}")
-    public Page<DutyResponse.DutyListDTO> DutyListByStatus(
+    public ResponseEntity<ApiResponse.Result<Page<DutyResponse.DutyListDTO>>> DutyListByStatus(
             @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size, @PathVariable String status) {
-        return dutyService.DutyListByStatus(page, size, status);
+        Page<DutyResponse.DutyListDTO> dutyListPage = dutyService.DutyListByStatus(page, size, status);
+        return ResponseEntity.ok(ApiResponse.success(dutyListPage));
     }
 }
