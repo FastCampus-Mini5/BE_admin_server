277ec3651657c7ed86e9610e81e745729a533b88
inyoung <charmingyear@gmail.com>
2023-07-28 15:42:37 +0900

    fix: 연차 신청 리스트 조회 반환값 수정 (#4)

--- src/main/java/com/adminServer/schedule/vacation/controller/VacationController.java
+++ src/main/java/com/adminServer/schedule/vacation/controller/VacationController.java
@@ -1,10 +1,13 @@
 package com.adminServer.schedule.vacation.controller;
 
+import com.adminServer._core.util.ApiResponse;
+import com.adminServer.schedule.duty.dto.DutyResponse;
 import com.adminServer.schedule.vacation.dto.VacationResponse;
 import com.adminServer.schedule.vacation.service.VacationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -16,8 +19,9 @@ public class VacationController {
     private final VacationService vacationService;
 
     @GetMapping("/{status}")
-    public Page<VacationResponse.VacationListDTO> VacationListByStatus(
+    public ResponseEntity<ApiResponse.Result<Page<VacationResponse.VacationListDTO>>> VacationListByStatus(
             @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size, @PathVariable String status) {
-        return vacationService.vacationListByStatus(page, size, status);
+        Page<VacationResponse.VacationListDTO> vacationListPage = vacationService.vacationListByStatus(page, size, status);
+        return ResponseEntity.ok(ApiResponse.success(vacationListPage));
     }
 }
