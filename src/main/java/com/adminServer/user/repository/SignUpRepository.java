07c0b9c47d8d384eb979a8e6e50c4e74aa129f90
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-07-29 19:07:44 +0900

    feat: 회원 가입 요청 테이블에서 이메일로 검색하는 기능 추가 (#9)

--- src/main/java/com/adminServer/user/repository/SignUpRepository.java
+++ src/main/java/com/adminServer/user/repository/SignUpRepository.java
@@ -3,5 +3,9 @@ package com.adminServer.user.repository;
 import com.adminServer.user.model.SignUp;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SignUpRepository extends JpaRepository<SignUp, Long> {
+
+    Optional<SignUp> findByEmail(String email);
 }
