0b37392297779b0729ae197f5476be905967c902
inyoung <charmingyear@gmail.com>
2023-07-29 19:09:19 +0900

    feat: 회원 이름, 이메일 복호화 조회 구현 (#5)

--- src/main/java/com/adminServer/schedule/duty/dto/DutyResponse.java
+++ src/main/java/com/adminServer/schedule/duty/dto/DutyResponse.java
@@ -1,5 +1,6 @@
 package com.adminServer.schedule.duty.dto;
 
+import com.adminServer._core.util.AESEncryptionUtil;
 import com.adminServer.schedule.duty.model.Duty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,20 +13,29 @@ public class DutyResponse {
     @NoArgsConstructor
     @AllArgsConstructor
     @Builder
-    public static class DutyListDTO {
+    public static class ListDTO {
 
         private String username;
         private String email;
         private Timestamp dutyDate;
         private Timestamp createdDate;
 
-        public static DutyListDTO toListDTO(Duty duty) {
-            return DutyListDTO.builder()
-                    .username(duty.getUser().getUsername())
-                    .email(duty.getUser().getEmail())
-                    .dutyDate(duty.getDutyDate())
-                    .createdDate(duty.getCreatedDate())
-                    .build();
+        public static ListDTO form(Duty duty) {
+            String decryptedUsername = null;
+            String decryptedEmail = null;
+            try {
+                decryptedUsername = AESEncryptionUtil.decrypt(duty.getUser().getUsername());
+                decryptedEmail = AESEncryptionUtil.decrypt(duty.getUser().getEmail());
+
+                return ListDTO.builder()
+                        .username(decryptedUsername)
+                        .email(decryptedEmail)
+                        .dutyDate(duty.getDutyDate())
+                        .createdDate(duty.getCreatedDate())
+                        .build();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
