a598427766b38ebef86b5e708ca5f03e04759bfe
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-07-29 19:08:11 +0900

    test: 회원 가입 승인 단위 테스트 작성 (#9)

--- src/test/java/com/adminServer/user/service/SignUpServiceTest.java
+++ src/test/java/com/adminServer/user/service/SignUpServiceTest.java
@@ -1,13 +1,19 @@
 package com.adminServer.user.service;
 
+import com.adminServer._core.errors.exception.EmptyDtoRequestException;
 import com.adminServer._core.errors.exception.EmptyPagingDataRequestException;
+import com.adminServer._core.errors.exception.SignUpServiceException;
+import com.adminServer.user.dto.SignUpRequest;
 import com.adminServer.user.dto.SignUpResponse;
 import com.adminServer.user.model.SignUp;
+import com.adminServer.user.model.User;
 import com.adminServer.user.repository.SignUpRepository;
+import com.adminServer.user.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -20,9 +26,13 @@ import org.springframework.data.domain.Pageable;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 class SignUpServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
 
     @Mock
     private SignUpRepository signUpRepository;
@@ -67,6 +77,50 @@ class SignUpServiceTest {
         // Then
         Assertions.assertThrows(EmptyPagingDataRequestException.class, () -> {
             signUpService.getAllList(null);
+        });
+    }
+
+    @DisplayName("회원 가입 요청 승인 성공 테스트")
+    @Test
+    void approve_Success_Test() {
+        // Given
+        SignUpRequest.ApproveDTO approveDTO = new SignUpRequest.ApproveDTO("user@example.com");
+        SignUp signUp = SignUp.builder().email("user@example.com").build();
+
+        Mockito.when(signUpRepository.findByEmail(approveDTO.getEmail()))
+                .thenReturn(Optional.of(signUp));
+
+        Mockito.when(userRepository.save(ArgumentMatchers.any(User.class)))
+                .thenReturn(new User());
+
+        // When
+        // Then
+        signUpService.approve(approveDTO);
+    }
+
+    @DisplayName("회원 가입 요청 승인 실패 테스트 - 비어있는 DTO")
+    @Test
+    void approve_Failure_Test_EmptyDTO() {
+        // Given
+        // When
+        // Then
+        Assertions.assertThrows(EmptyDtoRequestException.class, () -> {
+            signUpService.approve(null);
+        });
+    }
+
+    @DisplayName("회원 가입 요청 승인 실패 테스트 - 유효하지 않은 Email")
+    @Test
+    void approve_Failure_Test_InvalidEmail() {
+        // Given
+        Mockito.when(signUpRepository.findByEmail(ArgumentMatchers.anyString()))
+                .thenReturn(Optional.empty());
+
+        SignUpRequest.ApproveDTO approveDTO = new SignUpRequest.ApproveDTO("user@example.com");
+        // When
+        // Then
+        Assertions.assertThrows(SignUpServiceException.class, () -> {
+            signUpService.approve(approveDTO);
         });
     }
 }
