59dd7586a0aef692940da712f9dab40eb272c710
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-07-29 13:24:58 +0900

    feat: 회원가입 요청 리스트 응답 객체 구현 (#7)

---
+++ src/main/java/com/adminServer/user/dto/SignUpResponse.java
@@ -0,0 +1,30 @@
+package com.adminServer.user.dto;
+
+import com.adminServer.user.model.SignUp;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SignUpResponse {
+
+    @Getter
+    @Builder
+    public static class ListDTO {
+
+        private String username;
+        private String email;
+        private Timestamp hireDate;
+
+        public static ListDTO from(SignUp signUp) {
+            return ListDTO.builder()
+                    .username(signUp.getUsername())
+                    .email(signUp.getEmail())
+                    .hireDate(signUp.getHireDate())
+                    .build();
+        }
+    }
+}
