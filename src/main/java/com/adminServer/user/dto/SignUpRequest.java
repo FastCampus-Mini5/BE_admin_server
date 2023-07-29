1071780898cefd38c1c79d3b918417f0ef30abb1
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-07-29 19:01:35 +0900

    feat: 회원 가입 승인 처리 요청를 위한 DTO 생성 (#9)

---
+++ src/main/java/com/adminServer/user/dto/SignUpRequest.java
@@ -0,0 +1,21 @@
+package com.adminServer.user.dto;
+
+import com.adminServer._core.errors.ErrorMessage;
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SignUpRequest {
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    @ToString
+    public static class ApproveDTO {
+
+        @NotBlank(message = ErrorMessage.EMPTY_DATA_TO_APPROVE_SIGNUP)
+        private String email;
+    }
+}
