b8dd7a34a0e7f8bf90844470b8a25c95e978ff2f
inyoung <charmingyear@gmail.com>
2023-07-30 21:02:36 +0900

    feat: 당직 신청 승인 단위 테스트 구현 (#13)

--- src/test/java/com/adminServer/schedule/duty/service/DutyServiceTest.java
+++ src/test/java/com/adminServer/schedule/duty/service/DutyServiceTest.java
@@ -1,8 +1,10 @@
 package com.adminServer.schedule.duty.service;
 
 import com.adminServer._core.errors.exception.EmptyPagingDataRequestException;
+import com.adminServer._core.errors.exception.ScheduleServiceException;
 import com.adminServer._core.errors.exception.ValidStatusException;
 import com.adminServer.schedule.Status;
+import com.adminServer.schedule.duty.dto.DutyRequest;
 import com.adminServer.schedule.duty.dto.DutyResponse;
 import com.adminServer.schedule.duty.model.Duty;
 import com.adminServer.schedule.duty.repository.DutyRepository;
@@ -20,6 +22,7 @@ import org.springframework.data.domain.Pageable;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -90,6 +93,59 @@ class DutyServiceTest {
         assertThrows(EmptyPagingDataRequestException.class,
                 () -> dutyService.dutyListByStatus(pageable, validStatus));
         verify(dutyRepository, never()).findDutyByStatus(any(), any());
+    }
+
+    @DisplayName("당직 신청 승인 성공")
+    @Test
+    void testDutyApproveSuccess() {
+        //given
+        Long dutyId = 1L;
+        DutyRequest.StatusDTO statusDTO = new DutyRequest.StatusDTO(dutyId, "APPROVE");
+
+        Duty existDuty = createDuty(dutyId, createUser(1L, "user1"), "2023-07-01 00:00:00");
+        when(dutyRepository.findById(dutyId)).thenReturn(Optional.of(existDuty));
+
+        //when
+        dutyService.updateByStatus(statusDTO);
+
+        //then
+        verify(dutyRepository, times(1)).findById(dutyId);
+        verify(dutyRepository, times(1)).save(existDuty);
+
+        assertEquals(Status.APPROVE, existDuty.getStatus());
+    }
+
+    @DisplayName("당직 신청 승인 실패 - 유효하지 않은 상태")
+    @Test
+    void testDutyApproveFailWithStatus() {
+        //given
+        Long dutyId = 1L;
+        DutyRequest.StatusDTO statusDTO = new DutyRequest.StatusDTO(dutyId, "INVALID_STATUS");
+
+        Duty existingDuty = createDuty(dutyId, createUser(1L, "user1"), "2023-07-01 00:00:00");
+        when(dutyRepository.findById(dutyId)).thenReturn(Optional.of(existingDuty));
+
+        //when,then
+        assertThrows(ValidStatusException.class, () -> dutyService.updateByStatus(statusDTO));
+
+        verify(dutyRepository, times(1)).findById(dutyId);
+        verify(dutyRepository, never()).save(any());
+    }
+
+    @DisplayName("당직 신청 승인 실패 - 해당 당직 데이터가 없는 경우")
+    @Test
+    void testDutyApproveFailWithDTO() {
+        //given
+        Long dutyId = 1L;
+        DutyRequest.StatusDTO statusDTO = new DutyRequest.StatusDTO(dutyId, "APPROVE");
+
+        when(dutyRepository.findById(dutyId)).thenReturn(Optional.empty());
+
+        //when, then
+        assertThrows(ScheduleServiceException.class, () -> dutyService.updateByStatus(statusDTO));
+
+        verify(dutyRepository, times(1)).findById(dutyId);
+        verify(dutyRepository, never()).save(any());
     }
 
     private User createUser(Long id, String username) {
