34441be45ecd8f6a1a229615a582703db47a6606
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-07-30 16:26:21 +0900

    fix: 컨플릭트 처리 (#14)

--- src/main/java/com/adminServer/schedule/vacation/model/VacationInfo.java
+++ src/main/java/com/adminServer/schedule/vacation/model/VacationInfo.java
@@ -1,11 +1,16 @@
 package com.adminServer.schedule.vacation.model;
 
 import com.adminServer.user.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Table(name = "vacation_info_tb")
 @Entity
@@ -15,7 +20,7 @@ public class VacationInfo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
     private User user;
 
