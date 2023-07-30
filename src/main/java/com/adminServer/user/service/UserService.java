9647c5205270e41442ab8ac63f9e1f9ebcfad3d3
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-07-30 16:15:20 +0900

    feat: 연차 정보룰 포함한 전체 유저 조회 기능 구현 (#14)

---
+++ src/main/java/com/adminServer/user/service/UserService.java
@@ -0,0 +1,24 @@
+package com.adminServer.user.service;
+
+import com.adminServer._core.errors.exception.EmptyPagingDataRequestException;
+import com.adminServer.schedule.vacation.VacationInfo;
+import com.adminServer.user.dto.UserResponse;
+import com.adminServer.user.repository.VacationInfoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final VacationInfoRepository vacationInfoRepository;
+
+    public Page<UserResponse.ListDTO> getAllUsers(Pageable pageable) {
+        if (pageable == null) throw new EmptyPagingDataRequestException();
+
+        Page<VacationInfo> allUsersWithVacationInfo = vacationInfoRepository.findAll(pageable);
+        return allUsersWithVacationInfo.map(UserResponse.ListDTO::from);
+    }
+}
